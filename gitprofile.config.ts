01f3266e064a476184f72b453d65787f22b0d53f
valenzuelajanaa <162624758+valenzuelajanaa@users.noreply.github.com>
2024-04-11 11:03:41 -0700

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -77,7 +77,7 @@ const CONFIG = {
   },
   resume: {
     fileUrl:
-      'https://valenzuela.github.io/pdf/.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      //'https://valenzuela.github.io/pdf/.pdf', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
     'PCR', 'Bioinformatics', 'Teaching'
@@ -127,13 +127,13 @@ const CONFIG = {
     },  ],
   publications: [
  {
-      title: 'Effects of short-term experimental manipulation of captive social environment on uropygial gland microbiome and preen oil volatile composition',
+      title: '',
       conferenceName: '',
-      journalName: 'Frontiers in Ecology and Evolution',
-      authors: 'Danielle J. Whittaker, Amruth Atyam, Nathan A. Burroughs, Jonathan M. Greenberg, Travis J. Hagey, Milos V. Novotny, Helena A. Soini, Kevin R. Theis, Tricia A. Van Laar, and Joel W. G. Slade',
-      link: 'https://www.frontiersin.org/articles/10.3389/fevo.2022.1027399/full',
+      journalName: '',
+      authors: '',
+      link: '',
       description:
-        'Avian preen oil, secreted by the uropygial gland, is an important source of volatile compounds that convey information about the sender’s identity and quality, making preen oil useful for the recognition and assessment of potential mates and rivals. Although intrinsic factors such as hormone levels, genetic background, and diet can affect preen oil volatile compound composition, many of these compounds are not the products of the animal’s own metabolic processes, but rather those of odor-producing symbiotic microbes. Social behavior affects the composition of uropygial microbial communities, as physical contact results in microbe sharing. We experimentally manipulated social interactions in captive dark-eyed juncos (Junco hyemalis) to assess the relative influence of social interactions, subspecies, and sex on uropygial gland microbial composition and the resulting preen oil odor profiles',
+        '',
     },
     
       title: 'Publication Title',
